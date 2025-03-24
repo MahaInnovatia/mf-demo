ff61b9aa48a342c740cca26fe6f206502a6b2a85
Gowtham Ganesan <mail2gowthamganesan@gmail.com>
2025-03-24 15:02:46 +0530

    Initial commit

---
+++ host/src/App.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, NavLink } from 'react-router-dom';
+
+import './App.css';
+
+import UserTile from './UserTile';
+const AboutComponent = React.lazy(() => import('sharedMFE/AboutComponent'));
+const Dashboard = React.lazy(() => import('dashboard/Dashboard'));
+const UserManagement = React.lazy(() => import('userManagement/UserManagement'));
+const Header = React.lazy(() => import('header/Header'));
+import Footer from "./modules/Footer";
+import('sharedMFE/commonStyles');
+
+const App = () => (
+  <div className='common-container' style={{ backgroundColor: 'aqua' }}>
+    <div className='common-style'>Host MFE App</div>
+     <Suspense fallback={<div>Loading Header...</div>} >
+     <div style={{margin: '10px'}}>
+      <Header/>
+      </div>
+    </Suspense>
+    <div className="container">
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <nav>
+            <ul className="nav">
+              <li>
+                <NavLink to="/" end className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dash" end className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                  Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/image" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                  About
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <div style={{ display: 'flex', flexDirection: 'column', padding: '2px', gap: '20px' }}>
+                  <UserTile />
+                  <div style={{ display: 'flex', flexDirection: 'row', columnGap: '20px', justifyContent: 'space-around' }}>
+                    <div style={{ width: '50%' }}>
+                      <Dashboard />
+                    </div>
+                    <div style={{ width: '50%' }}>
+                      <UserManagement />
+                    </div>
+                  </div>
+                </div>
+              }
+            />
+            <Route
+              path="/dash"
+              element={<Dashboard />}
+            />
+            <Route
+              path="/image"
+              element={<AboutComponent />}
+            />
+          </Routes>
+
+        </Suspense>
+      </Router>
+    </div>
+    <div style={{margin: '10px'}}>
+    <Footer />
+    </div>
+  </div>
+);
+
+export default App;
